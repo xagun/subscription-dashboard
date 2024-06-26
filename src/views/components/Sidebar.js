9d421c1485619c8ae5ba1ae8d1796fdae8f6d0fd
xagun <xagun.shrestha@gmail.com>
2024-06-26 16:35:50 +0545

    feat: add sidebar with dashboard and user list link

--- src/views/components/Sidebar.js
+++ src/views/components/Sidebar.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+function useCheckActivePath(path) {
+  const location = useLocation();
+  return location.pathname === path;
+}
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar">
+        <ul>
+          <li>
+            <Link
+              to="/"
+              className={`sidebar-link ${
+                useCheckActivePath("/") ? "active-link" : ""
+              }`}
+            >
+              Dashboard
+            </Link>
+          </li>
+
+          <li>
+            <Link
+              to="/users"
+              className={`sidebar-link ${
+                useCheckActivePath("/users") ? "active-link" : ""
+              }`}
+            >
+              Users
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
