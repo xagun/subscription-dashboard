b8e24a354df7e1e25d656784bb0420a39bdffa44
xagun <xagun.shrestha@gmail.com>
2024-06-26 14:57:22 +0545

    chore: install sass in the project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Dashboard from "./views/Dashboard";
