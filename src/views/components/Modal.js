d22572811bccdbf350668ff147d9272daa22aa2c
xagun <xagun.shrestha@gmail.com>
2024-06-30 11:15:33 +0545

    fix: [x] modal close on pressing Escape [x] styling and responsiveness fixes

--- src/views/components/Modal.js
+++ src/views/components/Modal.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const Modal = ({ header, children, onClose }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onClose]);
   return (
     <div className="modal-overlay">
       <div className="modal">
